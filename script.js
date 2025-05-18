e3cb1df75e02f3a311f743e694f57396bf4beee1
pariawasthi <paridhiawasthi20@gmail.com>
2025-05-18 10:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar variables
+ */
+
+const navbar = document.querySelector("[data-nav]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+    elemToggleFunc(document.body);
+  })
+
+}
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+})
+ document.getElementById("livebtn").addEventListener("click",function(){
+  document.getElementById("backvideo").style.display="block";
+  document.getElementById("closebtn").style.display="block";
+  document.getElementById("livebtn").style.display="none";
+
+});
+document.getElementById("closebtn").addEventListener("click",function(){
+  document.getElementById("backvideo").style.display="none";
+  document.getElementById("closebtn").style.display="none";
+  document.getElementById("livebtn").style.display="block";
+
+});
+
+
+
+d
